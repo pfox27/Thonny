48144082c719053d607f68449b39a5f5b172cae7
pfox27 <80210760+pfox27@users.noreply.github.com>
2026-03-26 15:57:01 -0400

    update

--- Web_Server.py
+++ Web_Server.py
@@ -1,18 +1,81 @@
+import network
+import socket
+from time import sleep
+from picozero import pico_temp_sensor, pico_led
+import machine
+import rp2
+import sys
+
+ssid = 'MC-Staff'
+password = 'SSWst@ff2794!'
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        if rp2.bootsel_button() == 1:
+            sys.exit()
+        print('Waiting for connection...')
+        pico_led.on()
+        sleep(0.5)
+        pico_led.off()
+        sleep(0.5)
+        sleep(1)
+
+    print(wlan.ifconfig())
+    status = wlan.ifconfig()
+    ip = status[0] # Defines the 'ip' variable
+    pico_led.on()
+    return ip
+
+ip = connect()
+
+connect()
+
 def open_socket(ip):
     # Open a socket
     address = (ip, 80)
     connection = socket.socket()
     connection.bind(address)
     connection.listen(1)
-    print(connection)
+    return connection
+
+ip = connect()
+connection = open_socket(ip)
+
+def webpage(temperature, state):
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <form action="./lighton">
+            <input type="submit" value="Light on" />
+            </form>
+            <form action="./lightoff">
+            <input type="submit" value="Light off" />
+            </form>
+            <p>LED is {state}</p>
+            <p>Temperature is {temperature}</p>
+            </body>
+            </html>
+            """
+    return str(html)
+
+def serve(connection):
+    #Start a web server
+    state = 'OFF'
+    pico_led.off()
+    temperature = 0
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        print(request)
+        client.close()
 
 
 ip = connect()
-open_socket(ip)
-
-
-
-
-
-
-
+connection = open_socket(ip)
+serve(connection)
--- WiFiDisconnect.py
+++ WiFiDisconnect.py
@@ -1,7 +1,14 @@
+from picozero import pico_led
+import socket
+
 import network
 wlan = network.WLAN(network.STA_IF)
 wlan.disconnect()
 wlan.active(False)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+pico_led.off()
+sock.close()
+machine.reset()
 
 
 
