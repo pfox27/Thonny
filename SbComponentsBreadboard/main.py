4f6aa1c12ff1284bb551cdc7afcdffeb17a1589b
pfox27 <80210760+pfox27@users.noreply.github.com>
2026-04-14 08:32:47 -0400

    update

--- SbComponentsBreadboard/main.py
+++ SbComponentsBreadboard/main.py
@@ -7,15 +7,19 @@ LED2 = LED(1) #create object for LED class, LED connected at GP1
 LED3 = LED(2) #create object for LED class, LED connected at GP2
 
 BT1 = BUTTON(4) # Button connected at GP4
-buzzer = BUZZER(3) # Buzzer connected at GP8
+#buzzer = BUZZER(3) # Buzzer connected at GP8
 
 while 1:
    if BT1.read() == 1: #button pressed
        LED1.on()   #led 1 on
-       buzzer.on() # buzzer on
+       LED2.on()   #led 2 on
+       LED3.on()   #led 3 on
+       #buzzer.on() # buzzer on
        print("LED 1 ON")
    else :
        LED1.off()
+       LED2.off()
+       LED3.off()
        buzzer.off()
        print("Press Button 1")
        
